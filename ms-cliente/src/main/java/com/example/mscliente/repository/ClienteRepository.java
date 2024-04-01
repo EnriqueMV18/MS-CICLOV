396dc95f9e329dde6e14e33ec1a752bb96f9d93d
EnriqueMV18 <enriquemv937@gmail.com>
2024-03-31 19:28:11 -0500

    Eliminacion de archivos

--- ms-cliente/src/main/java/com/example/mscliente/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mscliente.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.mscliente.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
